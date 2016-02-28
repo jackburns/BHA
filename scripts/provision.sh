73b14be74e338836d4c8dc5c00f8c9639f4a32cf
raptiq <jack@raptiq.com>
2016-02-28 16:26:02 -0500

    fix local provisioning thigns

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -3,11 +3,13 @@
 # installing some easy stuff
 echo -e "\n--- Updating all the things ---\n"
 sudo apt-get -y update > /dev/null
+sudo apt-get -y upgrade > /dev/null
+
 echo -e "\n--- Install git nginx nodejs npm ---\n"
 sudo apt-get -y install git nginx nodejs npm
 echo -e "\n--- Install global npm packages"
 npm install -g gulp karma karma-cli webpack
-cd js
+cd /vagrant/js
 npm install
 cd ..
 
@@ -26,7 +28,6 @@ echo -e "\n--- Installing python and django ---\n"
 sudo apt-get -y install python3-pip
 sudo pip3 install django
 
-
 # setup mysql install
 echo -e "\n--- Setup and install MYSQL ---\n"
 sudo apt-get -y install debconf-utils
@@ -37,4 +38,4 @@ debconf-set-selections <<< "mysql-server mysql-server/root_password_again passwo
 
 apt-get install mysql-server -y
 
-service nginx restart
+sudo service nginx restart
