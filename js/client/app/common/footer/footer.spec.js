2f8f8eeb3a3cd30308ad6779966f590db326b8fa
Zach Gawlik <zachgawlik@gmail.com>
2016-03-22 14:07:21 -0400

    Test and implement volunteers table page

--- js/client/app/common/footer/footer.spec.js
+++ js/client/app/common/footer/footer.spec.js
@@ -27,11 +27,6 @@ describe('Footer', () => {
   });
 
   describe('Template', () => {
-    // template specs
-    // tip: use regex to ensure correct bindings are used e.g., {{  }}
-    it('has name in template [REMOVE]', () => {
-      expect(FooterTemplate).to.match(/{{\s?vm\.name\s?}}/g);
-    });
   });
 
   describe('Component', () => {
--- js/client/app/components/home/home.controller.js
+++ js/client/app/components/home/home.controller.js
@@ -1,6 +1,28 @@
 class HomeController {
   constructor() {
-    this.name = 'home';
+    const vm = this;
+
+    let makeVolunteer = function(id, firstName, lastName, languages) {
+      return {
+        id: id,
+        firstName: firstName,
+        lastName: lastName,
+        languages: languages
+      };
+    };
+
+    vm.ordering = 'lastName';
+    vm.isReverseOrder = false;
+    vm.updateOrder = function(ordering) {
+      vm.isReverseOrder = (vm.ordering === ordering) ? !vm.isReverseOrder : false;
+      vm.ordering = ordering;
+    };
+
+    vm.volunteers = [
+      makeVolunteer(0, 'Bill', 'Brown', ['German']),
+      makeVolunteer(1, 'Ellie', 'White', ['Portuguese, Spanish']),
+      makeVolunteer(2, 'Tom', 'Jones', ['Spanish', 'French'])
+    ];
   }
 }
 
