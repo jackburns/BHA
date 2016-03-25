6acfd1d26bfb06faa9cf5e534b314acd2ff9436d
Joseph Maxwell <joswellmaxeph@gmail.com>
2016-03-25 17:49:08 -0400

    more merge fixes

--- js/client/app/components/components.js
+++ js/client/app/components/components.js
@@ -1,19 +1,13 @@
 import angular from 'angular';
 import Home from './home/home';
 import About from './about/about';
-<<<<<<< HEAD
 import Login from './login/login';
-=======
->>>>>>> 6806fc6fbe1ccdf26e454c26e24ab666f1942eea
 import Vaf from './vaf/vaf';
 
 let componentModule = angular.module('app.components', [
   Home.name,
   About.name,
-<<<<<<< HEAD
-  Login.name
-=======
->>>>>>> 6806fc6fbe1ccdf26e454c26e24ab666f1942eea
+  Login.name,
   Vaf.name
 ]);
 
