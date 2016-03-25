94a06cb61fa19296680e69c1f8fc221b7b8a6677
Joseph Maxwell <joswellmaxeph@gmail.com>
2016-03-25 17:39:34 -0400

    forgot about 3:30PM

--- js/client/app/components/vaf/vaf.controller.js
+++ js/client/app/components/vaf/vaf.controller.js
@@ -85,7 +85,7 @@ class VafController {
     this.info.contact = "Phone";
     this.days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     this.times = ["9:00AM", "9:30AM", "10:00AM", "10:30AM", "11:00AM", "11:30AM", "12:00PM", "12:30PM", 
-                  "1:00PM", "1:30PM", "2:00PM", "2:30PM", "3:00PM", "4:00PM", "4:30PM", "5:00PM"];
+                  "1:00PM", "1:30PM", "2:00PM", "2:30PM", "3:00PM", "3:30PM", "4:00PM", "4:30PM", "5:00PM"];
 		  
     // check if availability is valid, set availabilities validity
     this.availf = function() {
