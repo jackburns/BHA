13006a7e2fafc7a3aa91de38aebbeb80f193cf5e
Zach Gawlik <zachgawlik@gmail.com>
2016-03-26 18:05:44 -0400

    Implement ui-select for languages

--- js/client/app/components/home/home.js
+++ js/client/app/components/home/home.js
@@ -1,9 +1,11 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import homeComponent from './home.component';
+import uiSelect from 'ui-select';
 
 let homeModule = angular.module('home', [
-  uiRouter
+  uiRouter,
+  uiSelect
 ]).config(['$stateProvider', '$urlRouterProvider', ($stateProvider, $urlRouterProvider) => {
 
   $urlRouterProvider.otherwise('/');
