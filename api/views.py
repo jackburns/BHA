0466b28e3761d98274d7b8cf46491aa854b7533a
Brian Cox <brian5021@verizon.net>
2016-03-27 00:26:25 -0400

    First attempt at Rest API

---
+++ api/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from api.serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
