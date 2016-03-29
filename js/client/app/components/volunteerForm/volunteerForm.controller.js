ce07c1ae737284c7efe7516702495664e15c2904
Joseph Maxwell <joswellmaxeph@gmail.com>
2016-03-29 18:53:52 -0400

    minor clean-up

--- js/client/app/components/volunteerForm/volunteerForm.controller.js
+++ js/client/app/components/volunteerForm/volunteerForm.controller.js
@@ -9,8 +9,8 @@ class VolunteerFormController {
       availabilityTimes: ["9:00AM", "9:30AM", "10:00AM", "10:30AM", "11:00AM", "11:30AM", "12:00PM", "12:30PM",
         "1:00PM", "1:30PM", "2:00PM", "2:30PM", "3:00PM", "3:30PM", "4:00PM", "4:30PM", "5:00PM"],
       carriers: ["AT&T", "Sprint", "T-Mobile", "Verizon", "All Tell", "Boost", "Cellular South", "Centennial Wireless",
-                 "Cincinnati Bell", "Cricket Wireless", "Metro PCS", "Powertel", "Qwest", "Rogers", "Suncom", "Telus",
-                 "U.S. Cellular", "Virgin Mobile USA", "Other"]
+        "Cincinnati Bell", "Cricket Wireless", "Metro PCS", "Powertel", "Qwest", "Rogers", "Suncom", "Telus",
+        "U.S. Cellular", "Virgin Mobile USA", "Other"]
     };
 
     this.name = 'Volunteer Application Form';
