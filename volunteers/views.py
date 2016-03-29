a879e6b88cbcbac1f5090b9f4cfe6cff8576596f
Tim Wright <wright.ti@husky.neu.edu>
2016-03-28 22:14:29 -0400

    Working on serializers, views

--- volunteers/views.py
+++ volunteers/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from .models import Volunteer
+from .serializers import VolunteerSerializer
 
-# Create your views here.
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+@csrf_exempt
+def get_volunteers(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    #if request.method == 'GET':
+    volunteers = Volunteer.objects.all()
+    serializer = VolunteerSerializer(volunteers, many=True)
+    return JSONResponse(serializer.data)
+"""
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)"""
