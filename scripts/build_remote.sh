615857150e999d70a3e4274d06c46175f388085d
raptiq <jack@raptiq.com>
2016-04-02 16:33:35 -0400

    derp use python3

--- scripts/build_remote.sh
+++ scripts/build_remote.sh
@@ -9,7 +9,7 @@ echo -e "\n--- Installing Python Packages ---\n"
 cd /data/www/BHA
 sudo pip3 install -r 'requirements.txt'
 
-python manage.py makemigrations
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 sudo uwsgi --emperor /etc/uwsgi/vassals --master --daemonize /var/log/uwsgi.log
 sudo service nginx restart
