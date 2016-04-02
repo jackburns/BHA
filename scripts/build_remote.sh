93da7a92975e77a546b3f158e532b8a31d29e18e
raptiq <jack@raptiq.com>
2016-04-02 15:55:45 -0400

    another one

--- scripts/build_remote.sh
+++ scripts/build_remote.sh
@@ -7,10 +7,9 @@ gulp webpack
 echo -e "\n--- Installing Python Packages ---\n"
 
 cd /data/www/BHA
-sudo pip3 install -r 'requirements.txt'
+sudo pip3 install -r -y 'requirements.txt'
 
 python manage.py makemigrations
 python manage.py migrate
-
 sudo uwsgi --emperor /etc/uwsgi/vassals --master --daemonize /var/log/uwsgi.log
 sudo service nginx restart
