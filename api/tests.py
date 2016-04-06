cc478e5ca2a752fcf8474016e91df28c48f69574
Tim Wright <wright.ti@husky.neu.edu>
2016-04-06 03:04:54 -0400

    wrote test specs for CRUD for volunteers, which we haven't finished yet. Thats how you do tdd folks

--- api/tests.py
+++ api/tests.py
@@ -3,11 +3,47 @@ from rest_framework.test import APIRequestFactory
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from django.test.client import Client
-import json
+from .models import Volunteer, Contact
+from django.utils import timezone
 
-# Using the standard RequestFactory API to create a form POST request
-factory = APIRequestFactory()
-request = factory.get('/api/volunteers/',  format='json')
+
+volunteer_jean = {
+            "contact": {
+                "street": "X Mansion",
+                "city": "Westchester",
+                "state": "NY",
+                "zip": "12345",
+                "phone_number": "1234567890",
+                "email": "timothywright11@gmail.com",
+                "preferred_contact": 0
+            },
+            "availability": [
+                {
+                    "day": 0,
+                    "start_time": "09:30:00",
+                    "end_time": "12:30:00"
+                }
+            ],
+            "languages": [
+                {
+                    "can_written_translate": False,
+                    "language_name": "da"
+                }
+            ],
+            "id": 1,
+            "first_name": "Jean",
+            "last_name": "Grey",
+            "sex": 1,
+            "volunteer_level": 0,
+            "inactive": False,
+            "hours": 0,
+            "notes": "",
+            "user": {
+                "id": 2,
+                "username": "example@example.com",
+                "is_staff": True
+            }
+}
 
 class ApiEndpointsTests(TestCase):
     def setUp(self):
@@ -34,25 +70,41 @@ class ApiEndpointsTests(TestCase):
         response = self.c.get("/admin/")
         self.assertEqual(response.status_code, 200)
 
-"""
-from .models import Volunteer
-from django.utils import timezone
-from django.core.urlresolvers import reverse
+    def create_contact(self):
+        return Contact.objects.create(email="example@example.com",
+                                      preferred_contact=1)
 
-# models test
-class WhateverTest(TestCase):
-
-    def create_volunteer(self, title="only a test", body="yes, this is only a test"):
+    def create_volunteer(self):
         return Volunteer.objects.create(first_name="John",
                                         last_name="Doe",
                                         sex=1,
-                                        created_at=timezone.now())
+                                        volunteer_level=1,
+                                        created_at=timezone.now(),
+                                        contact=self.create_contact())
 
-    def test_whatever_creation(self):
-        v = self.create_volunteer()
-        self.assertTrue(isinstance(v, Volunteer))
-        self.assertEqual(v.first_name, "John")
-        response = self.client.get('/api/volunteers/')
-        print(response)
+    def test_models(self):
+        con = self.create_contact()
+        vol = self.create_volunteer()
+        self.assertTrue(isinstance(con, Contact))
+        self.assertEqual(con.email, "example@example.com")
+        self.assertTrue(isinstance(vol, Volunteer))
+        self.assertEqual(vol.first_name, "John")
+        self.assertEqual(vol.last_name, "Doe")
+        self.assertEqual(vol.sex, 1)
+
+    def test_post(self):
+        response = self.c.post("/api/volunteers/", "volunteer_jean object")
         self.assertEqual(response.status_code, 200)
-"""
+        self.assertEqual(response.json()['count'], 1)
+
+    def test_delete(self):
+        response1 = self.c.post("/api/volunteers/", "add a volunteer object")
+        response2 = self.c.post("/api/volunteers/", "delete a volunteer objects")
+        self.assertEqual(response1.status_code, 200)
+        self.assertEqual(response2.status_code, 200)
+        self.assertEqual(response1.json()['count'], response2.json()['count']+1)
+
+    def test_update_volunteers(self):
+        response = self.c.post("/api/volunteers/", "data-for-updating-a-volunteer")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['whatever_field'], "updated value")
