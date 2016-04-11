ea61e8b66d20daefb5436ed1311dc28d06c771a0
raptiq <jack@raptiq.com>
2016-04-11 00:00:46 -0400

    remove unneeded crap from repo

--- js/client/app/components/resetPassword/resetPassword.spec.js
+++ js/client/app/components/resetPassword/resetPassword.spec.js
@@ -18,22 +18,6 @@ describe('ResetPassword', () => {
     // top-level specs: i.e., routes, injection, naming
   });
 
-  describe('Controller', () => {
-    // controller specs
-    it('has a name property [REMOVE]', () => { // erase if removing this.name from the controller
-      let controller = makeController();
-      expect(controller).to.have.property('name');
-    });
-  });
-
-  describe('Template', () => {
-    // template specs
-    // tip: use regex to ensure correct bindings are used e.g., {{  }}
-    it('has name in template [REMOVE]', () => {
-      expect(ResetPasswordTemplate).to.match(/{{\s?vm\.name\s?}}/g);
-    });
-  });
-
   describe('Component', () => {
       // component/directive specs
       let component = ResetPasswordComponent;
