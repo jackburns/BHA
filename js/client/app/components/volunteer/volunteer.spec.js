9fda9070c1ba1ddc86d40b3fe5ab86d097262f8a
raptiq <jack@raptiq.com>
2016-04-13 18:29:31 -0400

    remove old tests

--- js/client/app/components/volunteer/volunteer.spec.js
+++ js/client/app/components/volunteer/volunteer.spec.js
@@ -23,68 +23,6 @@ describe('Volunteer', () => {
 
   describe('Controller', () => {
 
-    // controller specs
-    it('has a volunteerId property', () => { 
-      let controller = makeController(0);
-      expect(controller.volunteerId).to.equal(0);
-
-      controller = makeController(1);
-      expect(controller.volunteerId).to.equal(1);
-
-      controller = makeController('1234');
-      expect(controller.volunteerId).to.equal(1234);
-    });
-
-    it('mocks a volunteer', () => {
-      let controller = makeController(0);
-
-      expect(controller.volunteer).to.exist;
-      expect(controller.volunteer.id).to.equal(0);
-      expect(controller.volunteer.firstName).to.equal("Bill");
-      expect(controller.volunteer.lastName).to.equal("Brown");
-      expect(controller.volunteer.languages).to.eql(["German"]);
-    });
-
-    it('generates a volunteer', () => {
-      let controller = makeController('19582');
-
-      expect(controller.volunteer).to.exist;
-
-      let volunteerParams = [
-        controller.volunteer.id,
-        controller.volunteer.firstName,
-        controller.volunteer.lastName,
-        controller.volunteer.languages,
-        controller.volunteer.birthday,
-        controller.volunteer.gender,
-        controller.volunteer.role,
-        controller.volunteer.volunteerLevel,
-        controller.volunteer.createdAt,
-        controller.volunteer.notes,
-        controller.volunteer.contact.street,
-        controller.volunteer.contact.city,
-        controller.volunteer.contact.state,
-        controller.volunteer.contact.zip,
-        controller.volunteer.contact.phoneNumber,
-        controller.volunteer.contact.email,
-        controller.volunteer.contact.preferredContact
-      ];
-
-      _(volunteerParams).forEach((param) => {
-        expect(param).to.exist;
-      });
-
-      expect(controller.volunteer.hours).to.be.within(30,60);
-      
-    });
-  });
-
-  describe('Template', () => {
-    it('displays name in template', () => {
-      expect(VolunteerTemplate).to.match(/{{\s?vm\.volunteer.firstName\s?}}/g);
-      expect(VolunteerTemplate).to.match(/{{\s?vm\.volunteer.middleName\s?}}/g);
-      expect(VolunteerTemplate).to.match(/{{\s?vm\.volunteer.lastName\s?}}/g);
-    });
   });
 
   describe('Component', () => {
