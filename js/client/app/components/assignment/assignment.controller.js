ed0543d7688ebd17322d4bc1736e7011d1b6ab77
Zach Gawlik <zachgawlik@gmail.com>
2016-04-25 19:49:31 -0400

    Filter assignments by status. Fix notify list

--- js/client/app/components/assignment/assignment.controller.js
+++ js/client/app/components/assignment/assignment.controller.js
@@ -1,19 +1,26 @@
 class AssignmentController {
-  constructor(Modals, Requests) {
+  constructor(Modals, Requests, User) {
 
     let getAssignments = () => {
-      Requests.getAssignments((results) => { this.futureAssignments = results;  })
+      Requests.getAssignments((results) => { this.assignments = results;  })
     };
 
     this.futureAssignments = [];
     getAssignments();
 
+    this.search = {status: 1};
+    this.isAdmin = User.isAdmin;
+    this.isReverseOrder = false;
+
     this.openNotificationsModal = (languageKey) => {
       Requests.getVolunteers({params: {language: languageKey}}, Modals.openNotifications)
     };
 
+    this.getVolunteersDisplay = (volunteers) => {
+      return _.map(volunteers, volunteer => volunteer.first_name + ' ' + volunteer.last_name).join(', ');
+    }
   }
 }
 
-AssignmentController.$inject = ['Modals', 'Requests'];
+AssignmentController.$inject = ['Modals', 'Requests', 'User'];
 export default AssignmentController;
