915c2c9087ba4981e9250891e39a2dce5c76270a
Kohlbrenner <Kohlbrenner.d@husky.neu.edu>
2016-10-20 02:51:40 -0400

    BHA-97:  Implement SSL support, added tests.
    BHA-96: Added a script for testing an SSL connection on url. Will alter url or ip as things change.
    
    Fixed the url in sslTest.py to point towards current instance
    
    Changed the url to point to the bha site where the app is going to be hosted.  Confirmed that they have their own SSL certificate implemented. Also edited test to work with Python 3.4

---
+++ tests/sslTest.py
@@ -0,0 +1,31 @@
+'''
+Tests for SSL connection. 
+On success, prints HTTP response information.
+On failure, prints HTTP failure information.
+Results are saved to test.log
+'''
+from urllib.request import urlopen
+import sys
+# replace url with whatever site it is currently being hosted on. Will update url.
+secure_url = 'https://www.bostonhousing.org/en/Job-Postings/Volunteer-Interpreter-positions.aspx'
+f = open('sslTest.log', 'w')
+try:
+	response = urlopen(secure_url)
+	result = 'PASSED: \n' + str(response.info())
+	f.write(result)
+	print(result)
+	f.close()
+except IOError as e:
+	if hasattr(e, 'code'):
+		result = 'FAILED: \n http error ', e.code
+		f.write(result)
+		print(result)
+	elif hasattr(e, 'reason'):
+		result = 'FAILED: \n url error. Site unreachable. ' + str(e.reason)
+		f.write(result)
+		print(result)
+	else:
+		result = 'FAILED: ', e
+		f.write(result)
+		print(result)
+	f.close()
