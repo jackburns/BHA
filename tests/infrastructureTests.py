6d240e17cc18cb024cd992d8e30880083f3e7240
Kohlbrenner <Kohlbrenner.d@husky.neu.edu>
2016-11-10 01:41:40 -0500

    BHA-112: fixed up the infrastructure tests, they now all pass on the live aws instance

--- tests/infrastructureTests.py
+++ tests/infrastructureTests.py
@@ -1,62 +1,41 @@
 '''
-Performing Infrastructure Tests with Testinfra framework
-
 packages to test for:
-	avahi-daemon
-	network-manager
-	virtualbox
-	python2.7
-	sendmail
-	nginx
+    sendmail
+    nginx
+    openssh
+    cloud-init
+    rpcbind
+    messagebus
 '''
 
-def test_avahidaemon_is_installed(Package):
-	avahi = Package("avahi-daemon")
-	assert avahi.is_installed
-
-def test_avahidaemon_running_and_enabled(Service):
-	avahi = Service("avahi-daemon")
-	assert avahi.is_running
-	assert avahi.is_enabled
-
-def test_networkManager_is_installed(Package):
-	networkManager = Package("network-manager")
-	assert networkManager.is_installed
-
-def test_networkManager_is_running_and_enabled(Service):
-	networkManager = Service("network-manager")
-	assert networkManager.is_running
-	assert networkManager.is_enabled
-
-def test_virtualbox_is_installed(Package):
-	virtualbox = Package("virtualbox")
-	assert virtualbox.is_installed
-
-def test_virtualbox_is_running_and_enabled(Service):
-	virtualbox = Service("virtualbox")
-	assert virtualbox.is_running
-	assert virtualbox.is_enabled
-
-def test_python27_is_installed(Package):
-	python27 = Package("python")
-	assert python27.is_installed
-	assert python27.version.startswith("2.7")
-
 def test_sendmail_is_installed(Package):
-	sendmail = Package("sendmail")
-	assert sendmail.is_installed
-
-def test_sendmail_is_running_and_enabled(Service):
-	sendmail = Service("sendmail")
-	assert sendmail.is_running
-	assert sendmail.is_enabled
-
-def test_nginx_is_installed(Package):
+    sendmail = Package("sendmail")
+    assert sendmail.is_installed
+    
+def test_nginx_is_installed(Package): 
     nginx = Package("nginx")
     assert nginx.is_installed
-
+    
 def test_nginx_running_and_enabled(Service):
-    nginx = Service("nginx")
+    nginx = Service("nginx") 
     assert nginx.is_running
-    assert nginx.is_enabled
 
+def test_opensshdaemon_is_installed(Package):
+    opensshdaemon = Package("openssh-server")
+    assert opensshdaemon.is_installed
+
+def test_cloudinit_running_and_enabled(Service):
+    cloudinit = Service("crond") 
+    assert cloudinit.is_running
+
+def test_rpcbind_is_installed(Package):
+    rpcbind = Package("rpcbind")
+    assert rpcbind.is_installed
+
+def test_rpcbind_running(Service):
+    rpcbind = Service("rpcbind") 
+    assert rpcbind.is_running
+
+def test_messagebus_running(Service):
+    messagebus = Service("messagebus") 
+    assert messagebus.is_running
